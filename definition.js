eb4e69e3e9496bdeafb6e0bd7c94e5511bbc9a8c
thientran <thientran@aitt.vn>
2020-03-25 11:19:29 +0700

    update

--- definition.js
+++ definition.js
@@ -1,4 +1,27 @@
 
+Blockly.Blocks['block_sound_firstplay'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        "type": "block_sound_firstplay",
+        "message0": "khởi động máy nghe nhạc",
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  }
+};
+Blockly.Python['block_sound_firstplay'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  Blockly.Python.definitions_['import_soundbit'] = 'from soundbit import *';
+  Blockly.Python.definitions_['create_sound'] = 'sound = SoundBit()';
+  var code = 'sound.first_play()\n';
+  return code;
+};
+
 Blockly.Blocks['block_sound_button'] = {
   init: function() {
     this.jsonInit(
@@ -12,20 +35,20 @@ Blockly.Blocks['block_sound_button'] = {
             "options": [
               [
                 "S1",
-                "1"
+                "15"
               ],
               [
                 "S2",
-                "2"
+                "14"
               ],
               [
                 "S3",
-                "3"
+                "13"
               ]
             ]
           }
         ],
-        "output": null,
+        "output": "Boolean",
         "colour": 230,
         "tooltip": "",
         "helpUrl": ""
@@ -36,8 +59,159 @@ Blockly.Blocks['block_sound_button'] = {
 Blockly.Python['block_sound_button'] = function(block) {
   var dropdown_nutnhan = block.getFieldValue('nutnhan');
   // TODO: Assemble Python into code variable.
-  var code = '...';
+  var code = 'pin'+dropdown_nutnhan+'.read_digital()==0';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
 
+Blockly.Blocks['block_sound_action'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        "type": "block_sound_action",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "action",
+            "options": [
+              [
+                {
+                  "src": "https://raw.githubusercontent.com/thienuittc/blocky_soundbit/master/icon-tian/PLAY.gif",
+                  "width": 50,
+                  "height": 30,
+                  "alt": "*"
+                },
+                "play"
+              ],
+              [
+                {
+                  "src": "https://raw.githubusercontent.com/thienuittc/blocky_soundbit/master/icon-tian/pause.gif",
+                  "width": 50,
+                  "height": 30,
+                  "alt": "*"
+                },
+                "pause"
+              ],
+              [
+                {
+                  "src": "https://raw.githubusercontent.com/thienuittc/blocky_soundbit/master/icon-tian/next.gif",
+                  "width": 50,
+                  "height": 30,
+                  "alt": "*"
+                },
+                "playNext"
+              ],
+              [
+                {
+                  "src": "https://raw.githubusercontent.com/thienuittc/blocky_soundbit/master/icon-tian/pre.gif",
+                  "width": 50,
+                  "height": 30,
+                  "alt": "*"
+                },
+                "playPrevious"
+              ]
+            ]
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  }
+};
+
+Blockly.Python['block_sound_action'] = function(block) {
+  var dropdown_action = block.getFieldValue('action');
+  // TODO: Assemble Python into code variable.
+  var code = 'soundbit.'+dropdown_action+'()\n';
+  return code;
+};
+
+Blockly.Blocks['block_sound_vol'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        "type": "block_sound_vol",
+        "message0": "mở âm lượng %1 %%",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "vol"
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  }
+};
+Blockly.Python['block_sound_vol'] = function(block) {
+  var number_vol = Blockly.Python.valueToCode(block, 'vol', Blockly.Python.ORDER_ATOMIC);
+  number_vol = number_vol/100;
+  // TODO: Assemble Python into code variable.
+  var code = 'sound.volume('+number_vol+')\n';
+  return code;
+};
+
+
+Blockly.Blocks['block_sound_sound'] = {
+  init: function() {
+    this.jsonInit(
+      {
+        "type": "block_sound_sound",
+        "message0": "đổi thể loại nhạc %1",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "class",
+            "options": [
+              [
+                "mặc định",
+                "0x00"
+              ],
+              [
+                "Rock",
+                "0x01"
+              ],
+              [
+                "Pop",
+                "0x02"
+              ],
+              [
+                "Jazz",
+                "0x03"
+              ],
+              [
+                "Classic",
+                "0x04"
+              ],
+              [
+                "Base",
+                "0x05"
+              ]
+            ]
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  }
+};
+
+Blockly.Python['block_sound_sound'] = function(block) {
+  var dropdown_class = block.getFieldValue('class');
+  // TODO: Assemble Python into code variable.
+  var code = 'sound.class_mucsic('+dropdown_class+')\n';
+  return code;
+};
