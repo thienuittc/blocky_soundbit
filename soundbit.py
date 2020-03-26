81d86dc67aaaeac47aca635a4447bfc1952ae889
thientran <thientran@aitt.vn>
2020-03-27 00:27:43 +0700

    fix

--- soundbit.py
+++ soundbit.py
@@ -129,8 +129,8 @@ class SoundBit():
 
     def volume(self, volume):
         self.awaitconfig()
-        self._volume = float(clamp(volume, 0, 1.0))
-        val = int(0.3 * self._volume)
+        self._volume = float(clamp(volume/100, 0, 1.0))
+        val = int(30.0 * self._volume)
         self.command(0x06,0x00 ,val)
         self.volumetime = ticks_ms()
 
